0ffd1054cba8699e68a49461365b5671e58e2821
Kliment Mamykin <kmamyk@gmail.com>
2020-11-29 17:42:20 +0000

    Added a paddle

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
 function preload() {
-    this.load.image('player', 'assets/repl.png');
+  this.load.image('player', 'assets/Pong Paddle.png');
 }
 
 function create() {
-    this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
-    this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
-    this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
-    this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+  this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+  this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+  this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+  this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
 
-    this.player = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(0.25, 0.25);
-    this.player.setCollideWorldBounds(true);
+  this.player = this.physics.add.image(config.width / 2, config.height, 'player').setScale(6, 4);
+  this.player.setCollideWorldBounds(true);
 }
 
 function update() {
-    let cursors = this.input.keyboard.createCursorKeys();
-    if ((cursors.left.isDown || this.a.isDown) || (cursors.right.isDown || this.d.isDown)) this.player.setVelocityX(cursors.left.isDown || this.a.isDown ? -160 : 160);
-    else this.player.setVelocityX(0);
-    if ((cursors.up.isDown || this.w.isDown) || (cursors.down.isDown || this.s.isDown)) this.player.setVelocityY(cursors.up.isDown || this.w.isDown ? -160 : 160);
-    else this.player.setVelocityY(0);
+  const hVelocity = 1000;
+  let cursors = this.input.keyboard.createCursorKeys();
+  if ((cursors.left.isDown || this.a.isDown) || (cursors.right.isDown || this.d.isDown)) this.player.setVelocityX(cursors.left.isDown || this.a.isDown ? -hVelocity : hVelocity);
+  else this.player.setVelocityX(0);
 }
 
 const config = {
-    type: Phaser.AUTO,
-    width: 500,
-    height: 400,
-    backgroundColor: '#f9f9f9',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y: 0
-            },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
+  type: Phaser.AUTO,
+  width: 500,
+  height: 600,
+  backgroundColor: '#000000',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 0
+      },
+      debug: true
     }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
 };
 
 const game = new Phaser.Game(config);
